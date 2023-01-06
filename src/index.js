6cd50f824560206dce347a4a4b918fef3cb85e27
djenifergurskas <djenifer.gurskas@warren.com.br>
2023-01-06 00:06:48 -0300

    commit
    commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,38 @@
-const express = require('express');
-const routes = require('./presentation/Routes');
+import express from "express"
+import router from "./routes/RoutesCustomer.js"
+import customerValidator from "./domain/user/valid/CustomerValidator.js"
+import CustomerModel from '../src/domain/user/valid/ModelUsers.js'
+import { validationResult } from "express-validator"
+import { customers } from "./mocks/CustomerMock.js"
 
-const app = express();
+const app = express()
 
 app.use(express.json())
-app.use(routes)
+app.use(router)
+
+app.get("/users", (req, res) => {
+  res.json(customers);
+});
+
+app.post("/create_user", customerValidator, (req, res,) => {
+  const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.mapped() })
+        }
+        var listCustomer = new CustomerModel(req.body);
+        customers.push(listCustomer);
+        res.status(200).json({ status: "success", customer_created: listCustomer });
+        
+})
+
+app.put("/update", (req, res) => {
+  res.json(customers)
+});
+
+app.delete("/delete", (req, res) => {
+  res.json(customers)
+});
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-})
+  console.log("Server is running on port 3000")
+});
---
+++ src/routes/RoutesCustomer.js
@@ -0,0 +1,22 @@
+import { customers } from '../mocks/CustomerMock.js'
+import express from "express"
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.json(customers);
+});
+
+router.post("/", (req, res) => {
+  res.json(customers);
+});
+
+router.put("/", (req, res) => {
+  res.json(customers);
+});
+
+router.delete("/", (req, res) => {
+  res.json(customers);
+});
+
+export default router
